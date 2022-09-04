c8d86ab171842e4a241bdb6412cb1a0fc65be0e8
Yuli016 <yulitsapantoja@gmail.com>
2022-09-03 23:26:55 -0700

    Updating Email

--- email.php
+++ email.php
@@ -10,148 +10,149 @@
   $recipient = "yulipantoja@gmail.com";
 
   mail($recipient, $subject, $message, $mailheader)
-  or die("Error has Occured! Please Refresh Page and try again");
+  or die("Error has Occured! Please refresh page and try again.");
 
-  echo
-  '
-  <!DOCTYPE html>
-  <html lang="en" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <title>Contact</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <!-- MDB -->
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.3.0/mdb.min.css" rel="stylesheet"/>
-      <script src="js/nav.js" defer></script>
+  echo'
 
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Roboto&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com">
-      <link rel="stylesheet" type="text/css" href="./css/background.css">
-      <link rel="stylesheet" type="text/css" href="./css/navbar.css">
-      <link rel="stylesheet" type="text/css" href="./css/footer.css">
-      <link rel="stylesheet" type="text/css" href="./css/page.css">
-      <link rel="stylesheet" type="text/css" href="./css/contact.css">
-    </head>
-    <body>
 
-      <nav id="navbar" class="">
-        <div class="nav-wrapper">
-          <div class="logo">
-            <a href="index.html"> <i class="fas fa-cat"></i> Yuli</a>
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>Contact</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- MDB -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.3.0/mdb.min.css" rel="stylesheet"/>
+        <script src="js/nav.js" defer></script>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" type="text/css" href="./css/background.css">
+        <link rel="stylesheet" type="text/css" href="./css/navbar.css">
+        <link rel="stylesheet" type="text/css" href="./css/footer.css">
+        <link rel="stylesheet" type="text/css" href="./css/page.css">
+        <link rel="stylesheet" type="text/css" href="./css/contact.css">
+      </head>
+      <body>
+
+        <nav id="navbar" class="">
+          <div class="nav-wrapper">
+            <div class="logo">
+              <a href="index.html"> <i class="fas fa-cat"></i> Yuli</a>
+            </div>
+
+            <ul id="menu">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="works.html">Works</a></li>
+              <li><a href="contact.html">Contact</a></li>
+              <li><a href="about.html">About</a></li>
+            </ul>
           </div>
+        </nav>
 
+        <!-- Menu Icon -->
+        <div class="menuIcon">
+          <span class="icon icon-bars"></span>
+          <span class="icon icon-bars overlay"></span>
+        </div>
+
+        <div class="overlay-menu">
           <ul id="menu">
             <li><a href="index.html">Home</a></li>
             <li><a href="works.html">Works</a></li>
             <li><a href="contact.html">Contact</a></li>
             <li><a href="about.html">About</a></li>
-          </ul>
-        </div>
-      </nav>
+            </ul>
+        </div><br><br>
 
-      <!-- Menu Icon -->
-      <div class="menuIcon">
-        <span class="icon icon-bars"></span>
-        <span class="icon icon-bars overlay"></span>
-      </div>
+        <div class="center-div">
 
-      <div class="overlay-menu">
-        <ul id="menu">
-          <li><a href="index.html">Home</a></li>
-          <li><a href="works.html">Works</a></li>
-          <li><a href="contact.html">Contact</a></li>
-          <li><a href="about.html">About</a></li>
-          </ul>
-      </div><br><br>
+          <div class="contact-form-wrapper d-flex justify-content-center">
 
-      <div class="center-div">
+            <form class="contact-form" action="email.php" method="POST">
 
-        <div class="contact-form-wrapper d-flex justify-content-center">
+              <h2 class="contact">Message Sent!</h2>
+              <p class="contact">Thank you for contacting me. I will get back to you as soon as possible!</p><br>
 
-          <form class="contact-form">
+            </form>
+          </div>
 
-            <h2 class="contact">Message Sent</h2>
-            <p class="contact">Thank you for contacting me. I'll get back to you as soon as soon as possible!</p><br><br>
-          </form>
         </div>
 
-      </div>
+        <footer class="text-center text-white">
 
-      <footer class="text-center text-white">
+          <!-- Section: Links  -->
+          <section class="">
+            <div class="container text-center text-md-start mt-5">
+              <!-- Grid row -->
+              <div class="row mt-3s">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4s">
+                  <!-- Content -->
+                  <h2 class="footer-title"><i class="fas fa-cat"></i> Yuli Pantoja</h2>
+                  <p class="contact"></p>
+                </div>
+                <!-- Grid column -->
 
-        <!-- Section: Links  -->
-        <section class="">
-          <div class="container text-center text-md-start mt-5">
-            <!-- Grid row -->
-            <div class="row mt-3s">
-              <!-- Grid column -->
-              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4s">
-                <!-- Content -->
-                <h2 class="footer-title"><i class="fas fa-cat"></i> Yuli Pantoja</h2>
-                <p class="contact"></p>
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4s">
+                  <!-- Links -->
+                  <h2 class="footer-title"> Links</h2>
+                  <nav id="footer-nav">
+                    <li><a href="index.html">Home</a></li><br>
+                    <li><a href="works.html">Works</a></li><br>
+                    <li><a href="contact.html">Contact</a></li><br>
+                    <li><a href="about.html">About</a></li>
+                  </nav>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4s">
+                  <!-- Links -->
+                  <h2 class="footer-title"> Contacts</h2>
+                  <p class="contact"><i class="fas fa-envelope"></i> yulipantoja@gmail.com</p>
+                  <p class="contact"><i class="fas fa-phone"></i> (831) 783-8856</p>
+                </div>
+                <!-- Grid column -->
               </div>
-              <!-- Grid column -->
-
-              <!-- Grid column -->
-              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4s">
-                <!-- Links -->
-                <h2 class="footer-title"> Links</h2>
-                <nav id="footer-nav">
-                  <li><a href="index.html">Home</a></li><br>
-                  <li><a href="works.html">Works</a></li><br>
-                  <li><a href="contact.html">Contact</a></li><br>
-                  <li><a href="about.html">About</a></li>
-                </nav>
-              </div>
-              <!-- Grid column -->
-
-              <!-- Grid column -->
-              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4s">
-                <!-- Links -->
-                <h2 class="footer-title"> Contacts</h2>
-                <p class="contact"><i class="fas fa-envelope"></i> yulipantoja@gmail.com</p>
-                <p class="contact"><i class="fas fa-phone"></i> (831) 755-8856</p>
-              </div>
-              <!-- Grid column -->
+              <!-- Grid row -->
             </div>
-            <!-- Grid row -->
-          </div>
-        </section>
-        <!-- Section: Links  -->
-
-        <!-- Grid container -->
-        <div class="container pt-4">
-          <!-- Section: Social media -->
-          <section class="mb-4s">
-
-            <!-- Itch.io -->
-            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://yuli0.itch.io/" target="_Itch" role="button" data-mdb-ripple-color="dark"><i class="fab fa-itch-io"></i></a>
-
-            <!-- Github -->
-            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://github.com/Yuli016" target="_Github" role="button" data-mdb-ripple-color="dark"><i class="fab fa-github"></i></a>
-
-            <!-- Linkedin -->
-            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://www.linkedin.com/in/yulitsa-pantoja/" target="_Linkedin" role="button" data-mdb-ripple-color="dark"><i class="fab fa-linkedin"></i></a>
-
           </section>
-          <!-- Section: Social media -->
-        </div>
-        <!-- Grid container -->
+          <!-- Section: Links  -->
 
-        <!-- Copyright -->
-          <div class="text-center p-3s">
-            © 2022 by Yuli Pantoja
+          <!-- Grid container -->
+          <div class="container pt-4">
+            <!-- Section: Social media -->
+            <section class="mb-4s">
+
+              <!-- Itch.io -->
+              <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://yuli0.itch.io/" target="_Itch" role="button" data-mdb-ripple-color="dark"><i class="fab fa-itch-io"></i></a>
+
+              <!-- Github -->
+              <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://github.com/Yuli016" target="_Github" role="button" data-mdb-ripple-color="dark"><i class="fab fa-github"></i></a>
+
+              <!-- Linkedin -->
+              <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://www.linkedin.com/in/yulitsa-pantoja/" target="_Linkedin" role="button" data-mdb-ripple-color="dark"><i class="fab fa-linkedin"></i></a>
+
+            </section>
+            <!-- Section: Social media -->
           </div>
+          <!-- Grid container -->
+
           <!-- Copyright -->
-      </footer>
+            <div class="text-center p-3s">
+              © 2022 by Yuli Pantoja
+            </div>
+            <!-- Copyright -->
+        </footer>
 
-    </body>
-  </html>
+      </body>
+    </html>
 
-  '
-  ;
+  ';
 
-?>
+  ?>
